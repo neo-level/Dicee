f575f37d948b2842e8714967382de26da1bb841a
neo-level <frederik@neo-level.com>
2023-07-16 17:30:06 +0200

    added array to store the dice. removed test code.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,13 +14,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var DiceImageViewLeft: UIImageView!
     @IBOutlet weak var DiceImageViewRight: UIImageView!
     
+    var dices = [ UIImage(named: "DiceOne"),
+                  UIImage(named: "DiceTwo"),
+                  UIImage(named: "DiceThree"),
+                  UIImage(named: "DiceFour"),
+                  UIImage(named: "DiceFive"),
+                  UIImage(named: "DiceSix") ]
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        DiceImageViewLeft.image = UIImage(named: "DiceSix")
-        DiceImageViewLeft.alpha = 0.5
-        
-        DiceImageViewRight.image = UIImage(named: "DiceTwo")
     }
     
     
